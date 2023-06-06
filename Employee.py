d96c24a8a232482389edfe6ba6d0e2cc3b0691e2
iwan <iwancuah13@gmail.com>
2023-06-06 20:21:34 +0700

    add Employee class

---
+++ Employee.py
@@ -0,0 +1,19 @@
+class Employee:
+    #Common base class for all employee
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print(f"Total Employee {self.empCount}")
+
+    def dislayEmployee(self):
+        print(f"Name : {self.name}, Salary : {self.salary}")
+
+
+empArief = Employee("Arief", 1000000)
+empArief.displayCount()
+empArief.dislayEmployee()
