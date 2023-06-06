29f9394870b29e8f594721e352e583649c3026e3
iwan <iwancuah13@gmail.com>
2023-06-06 19:36:22 +0700

    python class

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-class Emoployee:
-    #Common base class for all employees
+class Manusia:
+    #Class attribute
+    suku = "Jawa"
 
-    empCount = 0
+    def __init__(self, name, age, agama, jnskel, alamat, status):
+        self.name = name
+        self.age = age
+        self.jnskel = jnskel
+        self.agama = agama
+        self.alamat = alamat
+        self.status = status
 
-    def __init__(self) -> None:
-        pass
+    def biodata(self):
+        return f"Nama: {self.name} \nUsia: {self.age} \nAgama: {self.agama} \nJenis Kelamin: {self.jnskel} \nAlamat: {self.alamat} \nStatus: {self.status}"
+    
+test_arief = Manusia("Arief", 22, "Islam", "Laki-laki", "Pekalongan", "Jomblo")
+print(test_arief.biodata())
+print("Suku: ", test_arief.suku)
